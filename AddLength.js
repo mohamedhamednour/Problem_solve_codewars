4a7cd09a0134a0c4419b0c93fb18b4de33449c5a
mhamed <mohamedhame2ed@gmail.com>
2023-01-18 03:35:25 +0200

    add solve AddLength.js

---
+++ AddLength.js
@@ -0,0 +1,26 @@
+function addLength(str) {
+ return str.split(' ').map((item)=> `${item} ${item.length}`)
+
+ 
+  //start-here
+  }
+
+
+  console.log(addLength('mohamed hamed'))
+
+ 
+let arrays = {a:'hamed'}
+
+let add = [arrays, arrays ,arrays]
+add[1].a = 'mohamedhamed'
+add[0].a = 'vois'
+add[2].a = 'hamed'
+
+console.log(add)
+
+
+
+
+
+
+
